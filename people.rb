b520d2ee36bf6f39da48a787907e671cafd4ff82
Hira Kazmi <hirarukh@gmail.com>
2016-06-03 13:33:10 -0400

    created greeting method

--- people.rb
+++ people.rb
@@ -1,15 +1,33 @@
-class Instructor
 
-  def teach
+
+
+class People
+
+  def intitialize(name)
+    @name = name
+  end
+
+  def greeting
+    "Hi, my name is #{@name}"
+  end
+
+end
+
+class Instructor < People
+
+  def teach < people
     "Everything in Ruby is an Object"
   end
 
 end
 
-class Student
+class Student < People
 
   def learn
     "I get it"
   end
 
 end
+
+chris = People.new("Chris")
+puts chris.greeting

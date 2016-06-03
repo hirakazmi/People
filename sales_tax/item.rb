f21c2b53d794122ce3ddcadd850db9c381a4810e
Hira Kazmi <hirarukh@gmail.com>
2016-06-03 18:11:51 -0400

    made tax and total tax method

--- sales_tax/item.rb
+++ sales_tax/item.rb
@@ -1,4 +1,39 @@
-class Shopping_cart
+
+class Item
+
+attr_reader :calculated_tax
+
+  def initialize(name, shelf_price, sales_tax, import_tax)
+    @name = name
+    @shelf_price = shelf_price
+    @sales_tax = sales_tax
+    @import_tax = import_tax
+    @calculated_tax = 0
+  end
+
+  def tax
+    sales_tax = 0.10
+    import_tax = 0.05
+
+    if @sales_tax == true && @import_tax == true
+      @calculated_tax = @shelf_price * (sales_tax + import_tax)
+    elsif @sales_tax == true && @import_tax == false
+      @calculated_tax = @shelf_price * (@shelf_price + sales_tax)
+    elsif @sales_tax == false && @import_tax == true
+      @calculated_tax = @shelf_price + import_tax
+    elsif @sales_tax == false && @import_tax == false
+      @calculated_tax
+    end
+    return @calculated_tax.round(2)
+  end
+
+  def total_price
+    @shelf_price + tax
+  end
+
+end
+
+class Reciept
 
   def initialize
     @items = []
@@ -6,25 +41,25 @@ class Shopping_cart
 
   def add(item)
     @items << item
-    end
-end
-
-class Item
-
-  def initialize(name, shelf_price, sales_tax, import_tax)
-    @name = name
-    @shelf_price = shelf_price
-    @sales_tax = sales_tax
-    @import_tax = import_tax
   end
+
+  def printout
+
+
 end
 
-book = Item.new("book", 12.49, false, false )
 music_cd = Item.new("music_cd", 14.99, true, false)
+music_cd.tax
+puts music_cd.calculated_tax
 perfume = Item.new("perfume", 47.50, true, true)
+puts perfume.tax
+puts perfume.total_price
+book = Item.new("book", 12.49, false, false )
+puts book.tax
 
 
 shopping_cart1 =  Shopping_cart.new
 
+shopping_cart1.add(perfume)
 shopping_cart1.add(music_cd)
 shopping_cart1.add(book)

2acbe7feea79f0f6c8292d08cfd6cfaaa9c88cc0
Hira Kazmi <hirarukh@gmail.com>
2016-06-04 14:41:35 -0400

    finished reciept class

--- sales_tax/item.rb
+++ sales_tax/item.rb
@@ -1,14 +1,15 @@
 
 class Item
 
-attr_reader :calculated_tax
+attr_accessor :calculated_tax, :sales_tax, :import_tax, :shelf_price
+attr_reader :quantity, :name
 
-  def initialize(name, shelf_price, sales_tax, import_tax)
+  def initialize(quantity, name, shelf_price, sales_tax, import_tax)
+    @quantity = quantity
     @name = name
     @shelf_price = shelf_price
     @sales_tax = sales_tax
     @import_tax = import_tax
-    @calculated_tax = 0
   end
 
   def tax
@@ -16,15 +17,15 @@ attr_reader :calculated_tax
     import_tax = 0.05
 
     if @sales_tax == true && @import_tax == true
-      @calculated_tax = @shelf_price * (sales_tax + import_tax)
+      @shelf_price * (sales_tax + import_tax)
     elsif @sales_tax == true && @import_tax == false
-      @calculated_tax = @shelf_price * (@shelf_price + sales_tax)
+      @shelf_price + sales_tax
     elsif @sales_tax == false && @import_tax == true
-      @calculated_tax = @shelf_price + import_tax
+      @shelf_price + import_tax
     elsif @sales_tax == false && @import_tax == false
-      @calculated_tax
+      @shelf_price
+
     end
-    return @calculated_tax.round(2)
   end
 
   def total_price
@@ -35,31 +36,57 @@ end
 
 class Reciept
 
+  attr_accessor :items, :total, :total_sales_tax
+
   def initialize
     @items = []
+    @total = 0
+    @total_sales_tax = 0
   end
 
   def add(item)
-    @items << item
+    if item.is_a?(Item)
+      @items << item
+      @total_sales_tax += item.tax
+      @total += item.total_price
+    else
+      "Not a class Item"
+    end
   end
 
   def printout
+    items.each do |item|
+      p "#{item.quantity} #{item.name}: #{(item.shelf_price).round(2)}"
+    end
+      p "Sales Tax: #{(total_sales_tax).round(2)}"
+      p "Total: #{(total).round(2)}"
 
-
+  end
 end
 
-music_cd = Item.new("music_cd", 14.99, true, false)
-music_cd.tax
-puts music_cd.calculated_tax
-perfume = Item.new("perfume", 47.50, true, true)
-puts perfume.tax
-puts perfume.total_price
-book = Item.new("book", 12.49, false, false )
-puts book.tax
 
+book = Item.new( 1, "book", 12.49, false, false )
+music_cd = Item.new( 1, "music cd", 14.99, true, false)
+chocolate_bar = Item.new( 1, "chocolate bar", 0.85, false, false)
+box_of_chocolates = Item.new(1, "imported box of chocolate", 10.00, false, true)
+imported_perfume = Item.new(1, "imported bottle of perfume", 47.50, true, true)
+perfume = Item.new(1, "bottle of perfume", 20.89, true, false)
+headache_pills = Item.new(1, "packet of headache pills", 9.75, false, false)
 
-shopping_cart1 =  Shopping_cart.new
+reciept1 = Reciept.new
+reciept1.add(book)
+reciept1.add(music_cd)
+reciept1.add(chocolate_bar)
+reciept1.printout
 
-shopping_cart1.add(perfume)
-shopping_cart1.add(music_cd)
-shopping_cart1.add(book)
+reciept2 = Reciept.new
+reciept2.add(box_of_chocolates)
+reciept2.add(imported_perfume)
+reciept2.printout
+
+reciept3 = Reciept.new
+reciept3.add(perfume)
+reciept3.add(box_of_chocolates)
+reciept3.add(imported_perfume)
+reciept3.add(headache_pills)
+reciept3.printout

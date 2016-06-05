10b887d6f3eb3e85b3628f3a866aadf887930971
Hira Kazmi <hirarukh@gmail.com>
2016-06-05 14:19:39 -0400

    made rover class and initialize method

---
+++ mars_rover/mars.rb
@@ -0,0 +1,19 @@
+class Rover
+
+  def initialize(x_coordinate, y_coordinate, direction)
+    @x_coordinate = x_coordinate
+    @y_coordinate = y_coordinate
+    @direction = direction
+  end
+
+  def read_instruction
+    when @direction == "L"
+  end
+
+  def move
+  end
+
+  def turn
+  end
+
+end

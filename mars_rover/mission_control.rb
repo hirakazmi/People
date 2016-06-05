d8a2e05e21dfad3bbee64bc533bca6bd35e5210f
Hira Kazmi <hirarukh@gmail.com>
2016-06-05 17:46:07 -0400

    added gets questions

---
+++ mars_rover/mission_control.rb
@@ -0,0 +1,16 @@
+require_relative 'mars'
+
+puts "What is the size of the plateau?"
+plateau_position = gets.chomp
+
+puts "What is rovers start position and where is it facing?"
+start_position_and_direction = gets.chomp
+
+puts "What are rovers move instructions?"
+move_instructions = gets.chomp
+
+
+
+
+
+p plateau_position
